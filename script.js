c0842be84c7220dd90836a3cf363dca6844dec0c
Hintz-Madsen_JPRROI <rasmus.hintzmadsen@gmail.com>
2022-09-29 13:45:38 +0300

    Add chaining

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
-let displayValue = "";
-let pendingOperation = {};
-let lastOperation = undefined;
+let state = {};
+state.displayValue = "";
+state.pendingOperation = undefined;
+state.lastOperation = undefined;
+state.clearOnNextPress = false;
+state.memory = 0;
 let a = 0;
 let b = 0;
+
 const RESULT = document.querySelector(".result");
 
 // number key events
 const numbers = document.querySelectorAll(".number");
-console.log(numbers);
 for (const btn of numbers) {
 	btn.addEventListener("click", () => {
 		const digit = btn.innerText;
@@ -19,85 +22,119 @@ for (const btn of numbers) {
 const clearBtn = document.querySelector(".clear");
 clearBtn.addEventListener("click", () => {
 	setDisplayValue(0);
-	pendingOperation.operation = undefined;
-	pendingOperation.startInputtingB = false;
+	state.pendingOperation = undefined;
+	state.lastOperation = undefined;
+	state.clearOnNextPress = false;
+	state.inputtingB = false;
+	state.memory = 0;
+	a = 0;
+	b = 0;
 });
 
 // operator key events
 const operators = document.querySelectorAll(".operator");
 for (const btn of operators) {
 	btn.addEventListener("click", () => {
-		const operation = btn.classList[1];
-		a = displayValue;
-		pendingOperation.operation = operation;
-		pendingOperation.startInputtingB = true;
+		// START ADDING CHAINING FROM SOMEWHERE HERE
 
-		console.log(pendingOperation);
-		console.log(`a: ${a}`);
-		console.log(`b: ${b}`);
+		const btnOperation = btn.classList[1];
+		console.log(`click operator, pstate: ${state.pendingOperation}`);
+		if (state.pendingOperation) {
+			b = state.displayValue;
+			a = finalizePending();
+		} else {
+			a = state.displayValue;
+		}
+		state.pendingOperation = btnOperation;
+		state.clearOnNextPress = true;
+		state.inputtingB = true;
+
+		console.log(`after op, a: ${a}`);
+		console.log(`after op, b: ${b}`);
 	});
 }
 
 // equals key event
 const equalsBtn = document.querySelector(".equals");
 equalsBtn.addEventListener("click", () => {
-	let operation = pendingOperation.operation;
-	console.log(`operation: ${operation}`);
-	console.log(`lastOperation: ${lastOperation}`);
+	if (!state.inputtingB) {
+		return console.log("useless equals");
+	}
+	b = state.displayValue;
+	const result = finalizePending();
+	setDisplayValue(result);
+	state.inputtingB = false;
 
-	if (!operation && !lastOperation) {
+	/*
+	let operation = state.pendingOperation;
+
+	if (!operation && !state.lastOperation) {
 		return;
 	}
 
 	if (!operation) {
-		operation = lastOperation;
+		operation = state.lastOperation;
 	} else {
-		b = displayValue;
+		b = state.displayValue;
 		console.log("Set b to: " + b);
 	}
 
-	a = operate(operation, a, b);
+	const result = operate(operation, a, b);
+	console.log(`result: ${result}`);
+	a = result;
+	memory = Number(result);
+	console.log(`memory: ${state.memory}`);
 	console.log("newValue (a): " + a);
 	setDisplayValue(a);
-	pendingOperation.operation = undefined;
-	lastOperation = operation;
+	state.pendingOperation = undefined;
+	state.lastOperation = operation;
+    */
 });
 
 // functions
+function finalizePending() {
+	console.log(`finalizePending: `);
+	console.log(state);
+	console.log(`f a: ${a}`);
+	console.log(`f b: ${b}`);
+
+	const op = state.pendingOperation;
+	const result = operate(op, a, b);
+	a = result;
+	return result;
+}
+
 function numberPressed(number) {
-	if (displayValue == 0 || pendingOperation.startInputtingB) {
+	if (state.displayValue == 0 || state.clearOnNextPress) {
 		setDisplayValue(`${number}`);
-		pendingOperation.startInputtingB = false;
+		state.clearOnNextPress = false;
 	} else {
-		setDisplayValue(`${displayValue}${number}`);
+		setDisplayValue(`${state.displayValue}${number}`);
 	}
 }
 
 function setDisplayValue(value) {
-	console.log("Setting display to: " + value);
 	const length = String(value).length;
 	let fontSize = 96;
 
-	if (length == 17) {
-		return console.log("display full");
-	} else if (length > 17) {
-		return setDisplayValue("overflow");
-	}
+	// const factor = 1000000000;
+	// value = Math.round(value * factor) / factor;
 
 	const limit = 7;
-	console.log("Length: " + length);
 	if (length > limit) {
 		fontSize = 96 / (length / limit);
 	}
 
-	displayValue = value;
-	RESULT.innerText = displayValue;
+	state.displayValue = value;
+	RESULT.innerText = state.displayValue;
 	RESULT.style.fontSize = `${fontSize}px`;
 }
 
 function operate(operation, x, y) {
 	x = Number(x);
 	y = Number(y);
+
+	console.log(`operate: x: ${x} y: ${y}`);
 	if (typeof x != "number" || typeof y != "number") {
 		return console.log(`Not numbers: x: ${typeof x}, y: ${typeof y}`);
 	}
